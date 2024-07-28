870bbb96659473796f91ea3d5d118362eea6499f
siddu022 <sidduscout@gmail.com>
2024-07-28 20:33:25 +0530

    connected to mongodb

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB();
+const app = express();
+
+
+
+
+
+
+
+
+
+// (async ()=>{
+//   try {
+//    await mongoose.connect(`${process.env.MONGODB_URI}`)
+//     app.on("error", ()=>{
+//       console.log("ERRR: ",error);
+//       throw error
+//     })
+//
+//     app.listen(process.env.PORT, () =>{
+//       console.log(`App is listening on port
+//           ${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.error("ERROR: ",error)
+//     throw err
+//   }
+// })()
+
