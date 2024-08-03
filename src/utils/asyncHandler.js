4b9b4825a0e7ee03b5a59a969ed57832f381d559
siddu022 <sidduscout@gmail.com>
2024-08-03 16:44:24 +0530

    apiErrors,apiResponse handler files

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,36 @@
+
+const asyncHandler = (requestHandler) =>{
+      (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+  }
+
+}
+
+
+
+export {asyncHandler}
+
+
+//for better understanding  of we is happening here
+
+//const asyncHandler = () =>{}
+//const asyncHandler = (func) => () =>{}
+//const asyncHandler = (func) => async() =>{}
+
+
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//   try {
+//     await fn(req,res,next)
+//
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message
+//     })
+//   }
+// }
+
+
+
+
+
