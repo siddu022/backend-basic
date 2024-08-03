c677d89d4b3442b9d72eb1e04615357c634e4cb1
siddu022 <sidduscout@gmail.com>
2024-08-03 14:29:00 +0530

    connected db, written middleware in app.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ dotenv.config({
   path: './env'
 })
 
-connectDB();
-const app = express();
+connectDB().
+then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+.catch((err)=>{
+    console.log("mongo db connection failed!!",err);
+  })
 
 
 
